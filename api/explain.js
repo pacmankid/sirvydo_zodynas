a4f2a59b574c7d36cbe3630fa6bef047b26b956c
pacmankid <gedashdm@gmail.com>
2025-12-18 01:03:59 +0200

    Update explain.js

--- api/explain.js
+++ api/explain.js
@@ -40,8 +40,6 @@ module.exports = async function handler(req, res) {
 
     /* 3. Promptas DI – tik žodžio paaiškinimas */
     const promptToDI = `
-Paaiškink žodį „${word}“.
-
 ${contextText ? `Papildoma informacija iš žodyno:\n${contextText}` : ""}
 
 Instrukcijos:
